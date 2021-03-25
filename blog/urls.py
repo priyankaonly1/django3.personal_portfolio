f385d94a52b7e73696ddc2059565660a3958e8ca
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-03-25 17:48:11 +0530

    blog_model

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-# (. ka matlab h ki, blog ka views,agar kisi dusre app se views import karte to uska naam likhna padta)
+# (. ka matlab h ki, blog ka views,agar kisi dusre app se views import karte to uska naam likhna padta,or same app ho to . use karte h)
 
 urlpatterns = [
-    path('',views.all_blogs, name='all blogs'),
+    path('',views.all_blogs, name='all_blogs'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.order_by('-date')
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
