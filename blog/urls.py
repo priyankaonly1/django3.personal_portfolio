32e7a73269de8364a035e407f92f376e7e9d4648
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-03-25 20:42:08 +0530

    detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+app_name = 'blog'
 
 # (. ka matlab h ki, blog ka views,agar kisi dusre app se views import karte to uska naam likhna padta,or same app ho to . use karte h)
 
 urlpatterns = [
     path('',views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
